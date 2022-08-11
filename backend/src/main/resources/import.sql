732e5006e72bea76988710735de9c4014519a6ca
João Leahy <jvm.leahy@gmail.com>
2022-08-11 16:38:18 -0300

    Entity client and seeding database

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name,cpf,income,birth_Date,children) VALUES ('Rita Campos Oliveira','28794414041', 1900.0, '2000-09-04T10:30:00Z', 0);
+INSERT INTO tb_client (name,cpf,income,birth_Date,children) VALUES ('Bruno Bojack Crispim','62110892013', 2200.0, '2002-07-15T10:30:00Z', 2);
+INSERT INTO tb_client (name,cpf,income,birth_Date,children) VALUES ('Tiago Teixeira Melo','44098658038', 3400.0, '2001-05-02T11:02:00Z', 1);
+INSERT INTO tb_client (name,cpf,income,birth_Date,children) VALUES ('Miguel Santos Rosival','42657278086', 12100.0, '2003-06-14T07:30:00Z', 3);
+INSERT INTO tb_client (name,cpf,income,birth_Date,children) VALUES ('Roberto Goldberg Silva','38233422070', 500.0, '2004-01-24T16:02:00Z', 4);
+INSERT INTO tb_client (name,cpf,income,birth_Date,children) VALUES ('Jorge Silva Carvalho','07201617036', 1500.0, '1970-12-29T01:01:00Z', 4);
+INSERT INTO tb_client (name,cpf,income,birth_Date,children) VALUES ('Rafael Figueiredo Boaventura','25875798050', 1650.0, '1989-05-24T11:40:00Z', 1);
+INSERT INTO tb_client (name,cpf,income,birth_Date,children) VALUES ('Gilberto Gil Gomes','95913838009', 8200.0, '1980-09-16T16:16:16Z', 2);
+INSERT INTO tb_client (name,cpf,income,birth_Date,children) VALUES ('Caetano Veloso Barbosa','74446122002', 3000.0, '1999-04-22T10:30:00Z', 0);
+INSERT INTO tb_client (name,cpf,income,birth_Date,children) VALUES ('Matheus Ribeira Matos','97211408057', 1760.50, '2009-10-03T15:59:00Z', 1);
